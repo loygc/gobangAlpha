e6128a9f2c7d2110a2dab7a6a45f14bc5a73add7
Loyio <i@loyio.me>
2020-11-15 12:15:07 +0800

    first commit

---
+++ main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  gobangAlpha
+//
+//  Created by Loyio Macbook on 2020/11/14.
+//
+
+#include <iostream>
+#include "AlphaMain.hpp"
+
+int main(int argc, const char * argv[]) {
+    AlphaApp();
+    return 0;
+}
